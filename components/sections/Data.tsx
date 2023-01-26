6cbac77c721068d12b9e468d95a3f1a1a1eb3dd3
Ximena-21 <ximenanzola.21@gmail.com>
2023-01-26 16:20:40 -0500

    fixing: intersection observer data section and info section

--- components/sections/Data.tsx
+++ components/sections/Data.tsx
@@ -1,23 +1,46 @@
+import { useEffect, useRef, useState } from "react"
+
 export default function Data() {
 
+    const [dataYears, setDataYears] = useState(0)
+    const [dataWorks, setDataWorks] = useState(0)
+    const [dataShippings, setDataShippings] = useState(0)
 
-    
+
+    const myRef : any= useRef(null)
+
+    useEffect(() => {
+        const observerData = new IntersectionObserver(
+            (entries, observer) => {
+                if (entries[0].isIntersecting) {
+                    countDown(setDataYears, 20);
+                    countDown(setDataWorks, 400, true);
+                    countDown(setDataShippings, 5000, true);
+                    observer.unobserve(entries[0].target);
+                  }
+            },
+            { threshold: [0] }
+        );
+        
+        observerData.observe(myRef.current);
+    }, [])
+
     return (
-        <section className="data">
+        <section ref={myRef} className="data">
             <div className="cardData">
-                <h3 id='dataYears' className="cardData__number">0</h3>
+                <h3 id='dataYears' className="cardData__number">{dataYears}</h3>
                 <p className="cardData__text">Años</p>
                 <div className="cardData__subText">en el mercado</div>
                 <div className="cardData__line"></div>
             </div>
             <div className="cardData">
-                <h3 id='dataWorks' className="cardData__number">0</h3>
+                <h3 id='dataWorks' className="cardData__number">{dataWorks}</h3>
                 <p className="cardData__text">Obras</p>
                 <div className="cardData__subText">asesoradas</div>
                 <div className="cardData__line"></div>
             </div>
             <div className="cardData">
-                <h3 id='dataShippings' className="cardData__number">0</h3>
+                <h3 id='dataShippings' className="cardData__number">{dataShippings}</h3>
                 <p className="cardData__text">Despachos</p>
                 <div className="cardData__subText">realizados</div>
                 <div className="cardData__line"></div>
@@ -25,4 +48,24 @@ export default function Data() {
 
         </section>
     )
+}
+
+
+function countDown(setData: any, value: any, plusIcon = false, time = 2000) {
+    let counter = 0;
+
+    const fps = time / value > 10 ? time / value : 20;
+
+    let incrementValue = (fps * value) / time;
+
+    const timer = setInterval(() => {
+        counter += incrementValue;
+
+          setData ( plusIcon ? `+${counter}` : counter);
+
+        if (counter == value) {
+            clearInterval(timer);
+        }
+    }, fps);
+
 }
--- components/sections/Info.tsx
+++ components/sections/Info.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Image from 'next/image'
 
 import who from '../../src/assets/thoseWho.jpg'
@@ -8,31 +8,49 @@ import price from '../../src/assets/priceIcon.png'
 
 
 export default function Info() {
+
+    const myRef :any = useRef(null)
+
+    useEffect(()=>{        
+        const appearOptions = {
+            threshold: [0.5],
+            rootMargin: "0px 0px 0px 0px",
+          };
+          
+          const appearOnScroll = new IntersectionObserver((entries, observer) => {
+            // entries.forEach((entry) => {
+              if (entries[0].isIntersecting) {
+                entries[0].target.classList.add("appear");
+                observer.unobserve(entries[0].target);
+              }
+            // });
+          }, appearOptions);
+        //   document.querySelectorAll(".fade-in").forEach((fader) => {
+            appearOnScroll.observe(myRef.current);
+        //   });
+    }, [])
+
     return (
         <section id='nosotros' className="info">
-            {/* <img loading='lazy' src="../src/assets/quienesSomosImg.jpg" alt=""> */}
             <Image src={who} alt="" />
             <div className='info__imagesContainer'>
                 <div className='info__images'>
                     <div className='info__imagesItem'>
-                        <Image src={medal} alt="" />
-                        {/* <img src='../src/assets/medalIcon.png' alt=""> */}
+                        <Image src={medal} alt="medal" />
                         <p>CALIDAD</p>
                     </div>
                     <div className='info__imagesItem'>
-                        <Image src={time} alt="" />
-                        {/* <img src='../src/assets/timeIcon.png' alt=""> */}
+                        <Image src={time} alt="time" />
                         <p>CUMPLIMIENTO</p>
                     </div>
                     <div className='info__imagesItem'>
-                        <Image src={price} alt="" />
-                        {/* <img src='../src/assets/priceIcon.png' alt=""> */}
+                        <Image src={price} alt="price" />
                         <p>PRECIOS COMPETITIVOS</p>
                     </div>
                 </div>
             </div>
 
-            <div className="info__textBlock fade-in">
+            <div  ref={myRef} className="info__textBlock fade-in">
                 <h3 className="info__title">¿Quiénes Somos?</h3>
                 <p className="info__parrafo">Somos una ferretería Colombiana dedicada a la distribución y comercialización de herramientas y materiales para construcción, con una trayectoria de más de 15 años en el mercado del Municipio de Puerto Lleras, Meta</p>
                 <p className='info__parrafo'>Ofrecemos productos de calidad y precios justos, en búsqueda de satisfacer los requerimientos y necesidades de nuestros clientes.</p>
